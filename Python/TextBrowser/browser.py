1f5bb135035b3b5a0a98b9ad614ac964c2ba75d6
Dmitriy Sysoev <sysoevdmitriy@gmail.com>
2020-06-15 17:54:54 +0300

    work on browser project

--- Python/TextBrowser/browser.py
+++ Python/TextBrowser/browser.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import os
 from collections import deque
+from bs4 import BeautifulSoup
 import re
 
 def get_url(url_str):
@@ -13,14 +14,14 @@ def get_url(url_str):
 def is_file(usr_input_str):
     return '.' not in usr_input_str[::-1]
 
+
 def main():
-    dir = ""
+    usr_dir = ""
     page_story = deque()
     if len(sys.argv) >= 2:
-        dir = sys.argv[1]
-        dir_list = os.listdir()
-        if dir not in dir_list:
-            os.mkdir(dir)
+        usr_dir = sys.argv[1]
+        if not os.path.exists(usr_dir):
+            os.mkdir(usr_dir)
     url_content_prev = ""
     while True:
         usr_inp = input()
@@ -33,21 +34,30 @@ def main():
         else:
             if is_file(usr_inp):
                 try:
-                    with open(dir + "/" + usr_inp, "r") as file:
+                    with open(usr_dir + "/" + usr_inp, "r") as file:
                         for line in file:
                             print(line)
                 except IOError:
                     print("file not found")
             else:
                 url = get_url(usr_inp)
-                content = requests.get(url)
-                print(content.text)
+                page = requests.get(url)
+                soup = BeautifulSoup(page.content, 'html.parser')
+                tags = soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'ul', 'ol', 'li'])
+                content = []
+                for tag in tags:
+                    if len(tag.text) and tag.text[0] == '\n':
+                        continue
+                    content.append(tag.text)
+                content = "\n".join(content)
+                print(content)
+                # print(content.text)
                 if url_content_prev != "":
                     page_story.append(url_content_prev)
-                url_content_prev = content.text
+                url_content_prev = content
                 file_name = usr_inp[:-4]
-                with open(dir + "\\" + file_name, "w") as file:
-                    file.write(content.text)
+                with open(usr_dir + "\\" + file_name, "w") as file:
+                    file.write(content)
 
 
 if __name__ == "__main__":
